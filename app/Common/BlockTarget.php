563463b205d051f484c94c4838ce37db67d7f2b7
soras.k.m.tj16@gmail.com <soras.k.m.tj16@gmail.com>
2024-01-03 15:53:39 +0900

    ss

--- app/Common/BlockTarget.php
+++ app/Common/BlockTarget.php
@@ -104,7 +104,8 @@ class BlockTarget {
         if ($flg == 1) {
             $model = "sites";
         }
-        $name_lists = Auth::user()->$model()->orderBy('id', 'ASC')->get()->pluck('genre', 'name');
+        // $name_lists = Auth::user()->$model()->orderBy('id', 'ASC')->get()->pluck('genre', 'name');
+        $name_lists = Auth::user()->$model()->orderBy('id', 'ASC')->get();
         if ($model === "words") {
             foreach($name_lists as $name => $genre) {
                 fwrite($data, $name);
